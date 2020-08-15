895d4aabd25413a3f3c75679376dba6725f21074
Robert Houston <mastaginger@gmail.com>
2020-08-14 23:10:25 -0600

    physics client implemented 2

--- public/booth/booth.js
+++ public/booth/booth.js
@@ -13,7 +13,7 @@ function setup() {
   background(100);
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
-  socket = io.connect('http://localhost:3000');
+  socket = io.connect('/booth');
   // We make a named event called 'mouse' and write an
   // anonymous callback function
   socket.on('mouse',
